a6ffae6ef0364d3246238c36be9fdb8f2759779c
Faris <fariskarim2000@gmail.com>
2023-09-26 11:33:50 -0400

    refactor code out of app.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,12 @@ import SketchPad from "./components/Sketchpad";
 import { useState } from "react";
 
 function App() {
-  const rows = 20;
-  const cols = 20;
-  const [cells, setCells] = useState(Array(rows * cols).fill(''));
-
-  const handleClearAll = () => {
-    setCells(Array(rows * cols).fill(''));
-  };
-
-  const updateCells = (index) => {
-    setCells(cells.map((cell, i) => (i === index ? 'filled' : cell)));
-  };
   return (
     <>
       <div className="container">
         <h1>Sketchpad</h1>
       </div>
-      <div className="pad">
-        <button className="button" onClick={handleClearAll}>Clear all!</button>
-        <SketchPad rows={rows} cols={cols} cells = {cells} updateCells = {updateCells} />
-      </div>
+      <SketchPad />
     </>
   );
 }
--- src/components/Sketchpad.js
+++ src/components/Sketchpad.js
@@ -1,21 +1,43 @@
-import React, { useState } from 'react';
-import '../components/Sketchpad.css'
+import React, { useState } from "react";
+import "../components/Sketchpad.css";
 
-const SketchPad = ({ rows, cols, cells, updateCells }) => {
+const SketchPad = () => {
+  const rows = 30;
+  const cols = 30;
+  const [cells, setCells] = useState(Array(rows * cols).fill(""));
+
+  const updateCells = (index) => {
+    setCells(cells.map((cell, i) => (i === index ? "filled" : cell)));
+  };
+
   const handleMouseOver = (index) => {
     updateCells(index);
   };
 
+  const handleClearAll = () => {
+    setCells(Array(rows * cols).fill(""));
+  };
+
   return (
-    <div className="sketchpad">
-      {cells.map((cell, index) => (
-        <div
-          key={index}
-          className={`cell ${cell}`}
-          onMouseOver={() => handleMouseOver(index)}
-        />
-      ))}
-    </div>
+    <>
+      <div className="pad">
+        <div className="buttons">
+          <button className="button" onClick={handleClearAll}>
+            Clear all!
+          </button>
+          <button className="button change-color">Change color</button>
+        </div>
+        <div className="sketchpad">
+          {cells.map((cell, index) => (
+            <div
+              key={index}
+              className={`cell ${cell}`}
+              onMouseOver={() => handleMouseOver(index)}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
